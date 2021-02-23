b801a63da6ef74a2d520d4aae72eab7ccc9c0272
premakumaris <64866291+premakumaris@users.noreply.github.com>
2021-02-23 16:23:37 +0000

    SPFx connected

---
+++ SPFx-Connected/src/extensions/botExtension/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IBotExtensionApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'BotExtensionApplicationCustomizerStrings' {
+  const strings: IBotExtensionApplicationCustomizerStrings;
+  export = strings;
+}
